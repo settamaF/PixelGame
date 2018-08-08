21a284c996f26d139e42561ecf6cce5d20fbd8ac
f.settama <frederic.settama@gmail.com>
2018-08-08 18:00:44 +0200

    Check collision for generation

--- Assets/Scripts/Tools/BlockGenerator.cs
+++ Assets/Scripts/Tools/BlockGenerator.cs
@@ -11,7 +11,6 @@ public class BlockGenerator : Block
 {
 	#region Script Parameters
 	public GameObject   PrefabModel;
-	public Vector3Int   MaxSize = new Vector3Int(9, 9, 9);
 	public string		PathModel = "Assets/Data/Models";
 	public Model        ModelToGenerate;
 	#endregion
@@ -22,11 +21,15 @@ public class BlockGenerator : Block
 
 	#region Fields
 	// Const -------------------------------------------------------------------
-
+	private const int MAX_SIZE = 9;
+	public float OFFSET_COLLISION = 0.1f;
 	// Static ------------------------------------------------------------------
 
 	// Private -----------------------------------------------------------------
 	private GameObject mModel3D;
+	private MeshCollider mModel3DCollider;
+	private Vector3Int mSizeModel;
+	private List<Vector3> mValidCube;
 	#endregion
 
 	#region Unity Methods
@@ -37,11 +40,46 @@ public class BlockGenerator : Block
 	#endregion
 
 	#region Implementation
+	private GameObject mSphere;
+	private bool IsValidCube(float x ,float y, float z)
+	{
+		Vector3 pos = new Vector3(x > 0 ? x + OFFSET_COLLISION : x,
+								  y > 0 ? y + OFFSET_COLLISION : y,
+								  z > 0 ? z + OFFSET_COLLISION : z);
+
+		Debug.Log(pos);
+		Collider[] hitColliders = Physics.OverlapSphere(pos, 0.001f);
+		int i = 0;
+		if (hitColliders.Length == 0)
+			Debug.Log("Nothing");
+		while (i < hitColliders.Length)
+		{
+			Debug.Log(hitColliders[i].name);
+			i++;
+		}
+		mSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		mSphere.transform.position = pos;
+		mSphere.transform.localScale = new Vector3(.001f, .001f, .001f);
+		return false;
+		if (mSphere != null)
+			DestroyImmediate(mSphere);
+		bool ret;
+		
+		if (Physics.CheckSphere(pos, .001f))
+			ret = true;
+		else
+			ret = false;
+		mSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		mSphere.transform.position = pos;
+		mSphere.transform.localScale = new Vector3(.001f, .001f, .001f);
+		return ret;
+	}
+
 	private void SetupModel()
 	{
 		ModelToGenerate.Name = ModelToGenerate.name;
 		ModelToGenerate.Prefab = PrefabModel;
-		ModelToGenerate.Size = MaxSize;
+		ModelToGenerate.Size = mSizeModel;
 	}
 
 	private void SetupPositionModel3d()
@@ -54,27 +92,104 @@ public class BlockGenerator : Block
 			return;
 		}
 		Vector3 size = renderer.bounds.size;
+		mSizeModel = Vector3Int.FloorToInt(size);
 		Vector3 pos = size / 2;
 		pos.y = 0;
 		mModel3D.transform.position += pos;
 		renderer.transform.localPosition = Vector3.zero;
 	}
+
+	private void GenerateFullBlock()
+	{
+		GenerateBlock(mMaxSize);
+		foreach (var cube in mCubes)
+			cube.transform.SetParent(transform);
+	}
+
+	private void GenerateValidCube(int x, int y, int z)
+	{
+		var cube = GenerateCube(x, y, z);
+		cube.transform.SetParent(transform);
+		mCubes[x, y, z] = cube;
+		mValidCube.Add(new Vector3(x, y, z));
+	}
 	#endregion
 
 	#region Editor
-	[Button("Generation")]
+	[Button("Generation Model")]
 	private void GenerateModel()
 	{
 		ClearModel();
 		ModelToGenerate = Utils.CreateAsset<Model>(PathModel, PrefabModel.name);
+		mModel3D = Instantiate(PrefabModel, transform);
+		SetupPositionModel3d();
 		SetupModel();
 		Initialization();
 		SetupData(ModelToGenerate);
-		GenerateBlock(mMaxSize);
-		foreach (var cube in mCubes)
-			cube.transform.SetParent(transform);
+	}
+
+	[Button("Generation Max Block")]
+	private void GenerateBlock()
+	{
+		ClearModel();
+		ModelToGenerate = Utils.CreateAsset<Model>(PathModel, PrefabModel.name);
+		mSizeModel = new Vector3Int(MAX_SIZE, MAX_SIZE, MAX_SIZE);
+		SetupModel();
+		Initialization();
+		SetupData(ModelToGenerate);
 		mModel3D = Instantiate(mModel, transform);
 		SetupPositionModel3d();
+	}
+
+	[Button("GenerateValidCube")]
+	private void GenerateValidCube()
+	{
+		if (ModelToGenerate == null || mModel3D == null)
+		{
+			Debug.LogError("Generation cube is not initialized");
+			return;
+		}
+		mModel3DCollider = mModel3D.GetComponentInChildren<MeshCollider>();
+		if (mModel3DCollider == null)
+		{
+			Debug.LogError("No mesh collider found on the model 3D");
+			return;
+		}
+		mValidCube = new List<Vector3>();
+		for(int x = 0; x < mSizeModel.x; x++)
+		{
+			for (int y = 0; y < mSizeModel.y; y++)
+			{
+				for (int z = 0; z < mSizeModel.z; z++)
+				{
+					if (IsValidCube(x, y, z))
+					{
+						GenerateValidCube(x, y, z);
+						Debug.LogFormat("Ok {0} {1} {2}", x, y, z);
+					}
+					else
+					{
+						Debug.LogFormat("not ok {0} {1} {2}", x, y, z);
+					}
+				}
+			}
+		}
+		if (mValidCube != null && mValidCube.Count > 0)
+			ModelToGenerate.ValidCube = mValidCube.ToArray();
+	}
+
+	public Vector3 Value;
+	[Button("Test")]
+	private void Check()
+	{
+		if (IsValidCube(Value.x, Value.y, Value.z))
+		{
+			Debug.LogFormat("Ok {0} {1} {2}", Value.x, Value.y, Value.z);
+		}
+		else
+		{
+			Debug.LogFormat("not ok {0} {1} {2}", Value.x, Value.y, Value.z);
+		}
 	}
 
 	[Button("Clear Generator")]
@@ -85,8 +200,11 @@ public class BlockGenerator : Block
 			ClearScene();
 			return;
 		}
-		foreach(var cube in mCubes)
-			DestroyImmediate(cube.gameObject);
+		foreach (var cube in mCubes)
+		{
+			if(cube != null)
+				DestroyImmediate(cube.gameObject);
+		}
 		if (mModel3D)
 			DestroyImmediate(mModel3D);
 		mCubes = null;
@@ -99,6 +217,8 @@ public class BlockGenerator : Block
 		{
 			DestroyImmediate(child.gameObject);
 		}
+		if (transform.childCount > 0)
+			ClearScene();
 	}
 	#endregion
 }
---
+++ Assets/Scripts/Tools/CubeCollision.cs
@@ -0,0 +1,68 @@
+﻿//******************************************************************************
+// Authors: Frederic SETTAMA  
+//******************************************************************************
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCollision : MonoBehaviour
+{
+	#region Script Parameters
+
+	#endregion
+
+	#region Static
+
+	#endregion
+
+	#region Properties
+	public BlockGenerator Generator;
+#endregion
+
+#region Fields
+	// Const -------------------------------------------------------------------
+
+	// Static ------------------------------------------------------------------
+
+	// Private -----------------------------------------------------------------
+
+#endregion
+
+#region Unity Methods
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log(collision.collider.name);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log(other.name);
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		Debug.Log("Stay " + other.name);
+	}
+	#endregion
+
+	#region Methods
+
+	#endregion
+
+	#region Implementation
+
+	#endregion
+}
